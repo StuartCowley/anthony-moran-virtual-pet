2317104425d54080e0ebbab2086a40018c5ba307
Anthony Moran <tonymmoran@gmail.com>
2022-12-17 14:07:56 +0000

    changed to a class, used this.name

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,3 +5,15 @@ describe('constructor',() => {
         expect(new Pet ('Fido')).toBeInstanceOf(Object);
     });
 });
+
+describe('returns a name from an instance - my test',() => {
+    it('returns a name', () => {
+        expect(new Pet ('Fido')).toEqual({"name": "Fido"});
+    });
+});
+describe('constructor', () => {
+    it('sets the name property', () => {
+      const pet = new Pet('Fido');
+      expect(pet.name).toEqual('Fido');
+    });
+  });
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,18 @@
-function Pet() {}
-const newPetOne = new Pet('Fido')
+class Pet {
+    constructor(name) {                         //this is a constructor function 
+        this.name = name;                       //introducing 'this' 
+    }
+}
+const petOne = new Pet('Fido');                 //note capital P in Pet which references the class 'Pet'
+const petTwo = new Pet('Rover');                //passing the Rover argument into the name variable on line 2
+const petThree = new Pet('Gnasher');
+
 module.exports = Pet;
+
+// This is constructor function
+// function Pet(name) {
+//     this.name = name
+// }
+// const pet = new Pet('Fido');
+
+// module.exports = Pet;
