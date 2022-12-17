cf64646906e4d0f24e93617f6bb2feea79c6b901
Anthony Moran <tonymmoran@gmail.com>
2022-12-17 16:38:29 +0000

    added walk function and test

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,7 +5,6 @@ describe('constructor',() => {
         expect(new Pet ('Fido')).toBeInstanceOf(Object);
     });
 });
-
 // describe('returns a name from an instance - my test',() => {       
 //     it('returns a name', () => {
 //         expect(new Pet ('Fido')).toEqual({"name": "Fido"}); //this instance equals more than just name so errors.
@@ -16,17 +15,38 @@ describe('constructor', () => {
       const petOne = new Pet('Fido');
       expect(petOne.name).toEqual('Fido');
     });
-  });
+});
 describe('constructor', () => {
     it('has an initial age of 0', () => {
       const petOne = new Pet('Fido');
       expect(petOne.age).toEqual(0);
     });
+});
 describe('growUp', () => {
-        it('increments the age by 1', () => {
+    it('increments the age by +1, hunger by +5 and fitness by -3', () => {
           const petOne = new Pet('Fido');
-          pet.growUp();
-          expect(pet.age).toEqual(1);
-        });
-      }); 
+          petOne.growUp();
+          expect(petOne.age).toEqual(1);
+          expect(petOne.hunger).toEqual(5);
+          expect(petOne.fitness).toEqual(7);
+    });  
+});
+describe('walk', () => {
+    it('increments the fitness by +4', () => {
+          const petOne = new Pet('Fido');
+          petOne.walk();
+          expect(petOne.fitness).toEqual(14);
+    });  
+});
+describe('constructor', () => {
+    it('has an initial hunger of 0', () => {
+      const petOne = new Pet('Fido');
+      expect(petOne.hunger).toEqual(0);
+    });
+});
+describe('constructor', () => {
+    it('has an initial fitness of 10', () => {
+      const petOne = new Pet('Fido');
+      expect(petOne.fitness).toEqual(10);
+    });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,23 @@
 class Pet {
-    constructor(name, age) {                         //this is a constructor function 
-        this.name = name;                       //introducing 'this' 
-        this.age = 0;
+    constructor(name) {
+        {
+            this.name = name; //introducing 'this' function
+            this.age = 0;
+            this.hunger = 0;
+            this.fitness = 10;
+        }
+        Pet.prototype.growUp = function () {
+            this.age += 1;
+            this.hunger += 5;
+            this.fitness -= 3;
+        }
+        Pet.prototype.walk = function () {
+            this.fitness += 4;
+        };
     }
-    Pet.prototype.growUp = function() {
-      this.age += 1;
-    };
 }
 const petOne = new Pet('Fido');            //1st instance. //note capital P in Pet which references the class 'Pet'
-const petTwo = new Pet('Rover');           //2nd instance. //passing the Rover argument into the name variable on line 2
+const petTwo = new Pet('Rex');           //2nd instance. //passing the Rover argument into the name variable on line 2
 const petThree = new Pet('Gnasher');
 
 module.exports = Pet;
