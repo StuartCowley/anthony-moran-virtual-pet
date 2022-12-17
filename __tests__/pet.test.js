4e14f94ff7edeb5dbd9c8e90d9228a53c4856b7f
Anthony Moran <tonymmoran@gmail.com>
2022-12-17 15:24:11 +0000

    added this age functionality

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -6,14 +6,27 @@ describe('constructor',() => {
     });
 });
 
-describe('returns a name from an instance - my test',() => {
-    it('returns a name', () => {
-        expect(new Pet ('Fido')).toEqual({"name": "Fido"});
-    });
-});
+// describe('returns a name from an instance - my test',() => {       
+//     it('returns a name', () => {
+//         expect(new Pet ('Fido')).toEqual({"name": "Fido"}); //this instance equals more than just name so errors.
+//     });
+// });
 describe('constructor', () => {
     it('sets the name property', () => {
-      const pet = new Pet('Fido');
-      expect(pet.name).toEqual('Fido');
+      const petOne = new Pet('Fido');
+      expect(petOne.name).toEqual('Fido');
     });
   });
+describe('constructor', () => {
+    it('has an initial age of 0', () => {
+      const petOne = new Pet('Fido');
+      expect(petOne.age).toEqual(0);
+    });
+describe('growUp', () => {
+        it('increments the age by 1', () => {
+          const petOne = new Pet('Fido');
+          pet.growUp();
+          expect(pet.age).toEqual(1);
+        });
+      }); 
+});
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,14 @@
 class Pet {
-    constructor(name) {                         //this is a constructor function 
+    constructor(name, age) {                         //this is a constructor function 
         this.name = name;                       //introducing 'this' 
+        this.age = 0;
     }
+    Pet.prototype.growUp = function() {
+      this.age += 1;
+    };
 }
-const petOne = new Pet('Fido');                 //note capital P in Pet which references the class 'Pet'
-const petTwo = new Pet('Rover');                //passing the Rover argument into the name variable on line 2
+const petOne = new Pet('Fido');            //1st instance. //note capital P in Pet which references the class 'Pet'
+const petTwo = new Pet('Rover');           //2nd instance. //passing the Rover argument into the name variable on line 2
 const petThree = new Pet('Gnasher');
 
 module.exports = Pet;
